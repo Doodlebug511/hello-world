9e3e496cf4e76f9b3e6d65622cac55493a4d9609
Doodlebug511 <kedaniels511@gmail.com>
2021-12-19 19:47:15 -0800

    ran this code through Atom linter

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # hello-world
 just getting started, learning as I go
-got a BS in computer science a few years ago, but have no friends who like this kind of stuff.  would like to code for 
-a living.  completed a data science bootcamp, leaning towards that now. 
+got a BS in computer science a few years ago, but have no friends who like this
+kind of stuff.  would like to code for a living.  completed a data science data
+boot-camp, leaning towards that now.
+
+Been away for a while, completed several boot-camps in python, data science,
+SQL, web scaping, C++, etc.  Will get around to improving this project while I
+pursue others.  Looking to contribute to open source and volunteer to gain
+experience.
--- myscraper.py
+++ myscraper.py
@@ -1,51 +1,70 @@
+# after python install, pip install these 2 libraries from the command prompt
+# while online then import as shown
 
-#after python install, pip install these 2 libraries from the command prompt while online
-#then import as shown
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-#bring desired webpage into python and create soup object via beautifulsoup
-r = requests.get('https://pauladeen.com/recipe/mashed-potatoes-with-sautaed-mushrooms/')
-#r = requests.get('https://pauladeen.com/recipe/fried-bacon-mashed-potatoes/')
-#r = requests.get('https://pauladeen.com/recipe/shrimp-mashed-potatoes/')
+# bring desired webpage into python and create soup object via beautifulsoup
 
-#different parsers for different websites and output, these are the main ones
+url = 'https://pauladeen.com/recipe/mashed-potatoes-with-sautaed-mushrooms/'
+# url = 'https://pauladeen.com/recipe/fried-bacon-mashed-potatoes/'
+# url = 'https://pauladeen.com/recipe/shrimp-mashed-potatoes/'
+r = requests.get(url)
+
+# different parsers for different websites and output, these are the main ones
 soup = BeautifulSoup(r.text, 'html.parser')
-#soup = BeautifulSoup(r.text, 'lxml.parser') #will need a pip install of lxml first
-#soup = BeautifulSoup(r.text, 'html5lib.parser') #pip install of html5lib first
 
-records = []  #main container for soup object   
+# soup = BeautifulSoup(r.text, 'lxml.parser')
+# will need a pip install of lxml first
 
-records.append(soup.find('title').text) #adding title text to records array
-records.append(' ')                     #adding spacer between texts for readability 
-print(soup.find('title').text)          #print to see what I've got
+# soup = BeautifulSoup(r.text, 'html5lib.parser')
+# pip install of html5lib first
 
-pic = soup.find('img', class_='img-fluid').get('data-src')  #url of image
+# main container for soup object
+records = []
+
+# adding title text to records array
+records.append(soup.find('title').text)
+
+# adding spacer between texts for readability
+records.append(' ')
+
+# print to see what I've got
+print(soup.find('title').text)
+
+# url of image
+pic = soup.find('img', class_='img-fluid').get('data-src')
+
+# print to see what I've got
 print(pic)
+
 records.append(pic)
 records.append(' ')
 
+# list of ingredients
 ingredients = []
-for results1 in soup.find_all('li', itemprop='ingredients'): #list of ingredients
+for results1 in soup.find_all('li', itemprop='ingredients'):
+    # print to see what I've got
     print(results1.text)
+
     ingredients.append(results1.text)
     records.append(results1.text)
-records.append(' ')     
+records.append(' ')
 
+# list of instructions
 directions = []
-for results in soup.find_all('p'):  #list of instructions
+for results in soup.find_all('p'):
+    # print to see what I've got
     print(results.text)
+
     directions.append(results.text)
     records.append(results.text)
-records.append(' ')    
+records.append(' ')
 
-#pip install pandas, then import
-import pandas as pd
-
-df = pd.DataFrame(records)  #create data frame object out of records
+# create data frame object out of records
+df = pd.DataFrame(records)
 records
-#send to folder of source file as .csv file, open with spreadsheet app(excel)
-df.to_csv('pdRecipies.csv', index = False, encoding='utf-8') 
 
-
-
+# send to folder of source file as .csv file, open with spreadsheet app(excel)
+df.to_csv('pdRecipies.csv', index=False, encoding='utf-8')
